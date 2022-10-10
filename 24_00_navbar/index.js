9f265896d4f0547f3c63a653c4671f542cf30fd5
moog.kwon <diligentaf@gmail.com>
2022-10-10 18:04:14 +0900

    navbar project done

--- 24_00_navbar/index.js
+++ 24_00_navbar/index.js
@@ -35,15 +35,114 @@ function Navbar() {
 }
 
 function MainContent() {
-  return(
+  return (
     <h1>I'm learning React!</h1>
   )
 }
 
-ReactDOM.render(
+const page = (
   <div>
     <Navbar />
     <MainContent />
   </div>
+)
+
+ReactDOM.render(
+  page
   , document.getElementById("root")
+)
+
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className = "header"
+document.getElementById("root").append(h1)
+
+const element = <h1 className="header">This is JSX</h1>
+console.log(element)
+
+ReactDOM.render(
+  element
+  , document.getElementById("thisjsx")
+)
+
+const navbar = (
+  <nav>
+    <h1>Bob's Bistro</h1>
+    <ul>
+      <li>pricing</li>
+      <li>about</li>
+      <li>contact</li>
+    </ul>
+  </nav>
+)
+
+ReactDOM.render(
+  navbar
+  , document.getElementById("navbar")
+)
+
+function Footer() {
+  return (
+    <footer>
+      <small>ⓒ 2021 sigma development. All rights reserved</small>
+    </footer>
+  )
+}
+
+function Header() {
+  return(
+    <header>
+      <nav className="nav">
+        {/* <h1>Reasons I'm excited to learn React</h1> */}
+        <img src="./react_logo.png" className="nav-logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+function ReactImage() {
+  return (
+    <div>
+      <Header />
+      <img src="./react_logo.png" width="40px" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k starts on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+      <Footer />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <ReactImage />
+  // ReactImage() //MOOG: this also works.. Only works if the funciton is pascal case (not camel case)
+  , document.getElementById("reactReactImage")
+)
+
+const page2 = (
+  <div>
+    <h1>My awesome website in React</h1>
+    <h3>Reasons I love React</h3>
+    <ol>
+      <li>It's composable</li>
+      <li>It's declarative</li>
+      <li>It's a hireable skill</li>
+      <li>It's actively maintained by skilled people</li>
+    </ol>
+  </div>
+)
+
+ReactDOM.render(
+  page2
+  , document.getElementById("stringify").append(JSON.stringify(page2))
 )
