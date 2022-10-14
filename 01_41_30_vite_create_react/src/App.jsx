247a47f8bd22fa50feddcd48ecd411f40c021afb
moog.kwon <diligentaf@gmail.com>
2022-10-14 15:42:28 +0900

    updated link

--- 01_41_30_vite_create_react/src/App.jsx
+++ 01_41_30_vite_create_react/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="ayyhttps://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
