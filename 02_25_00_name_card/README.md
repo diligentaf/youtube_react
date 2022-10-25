a59cffb21a560f47a17aaee31be5d8368561fbbb
moog.kwon <diligentaf@gmail.com>
2022-10-25 13:51:52 +0900

    props

---
+++ 02_25_00_name_card/README.md
@@ -0,0 +1,1 @@
+https://youtu.be/bMknfKXIFA8?t=14001
--- 02_25_00_name_card/src/components/Navbar.jsx
+++ 02_25_00_name_card/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
   return (
     <nav>
-      <img src="../public/airbnb_logo.png" className="nav--logo" />
+      <img src="../airbnb_logo.png" className="nav--logo" />
     </nav>
   )
 }
